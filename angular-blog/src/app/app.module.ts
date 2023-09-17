11800671cda968b9c5c6000b50a94aa6497283f9
Rodolfo Guerra <rodolfoguerraster@gmail.com>
2023-09-16 21:44:36 -0300

    feat: criado os componentes a partir do layout

--- angular-blog/src/app/app.module.ts
+++ angular-blog/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { TitleComponent } from './components/title/title.component';
+import { DetailsComponent } from './components/details/details.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    TitleComponent,
+    DetailsComponent,
+    CardComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
