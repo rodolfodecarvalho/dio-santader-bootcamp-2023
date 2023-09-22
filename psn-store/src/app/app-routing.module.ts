65691ad4fecb8c7e4bb515bfb74cfe3e6fbb6b72
Rodolfo Guerra <rodolfoguerraster@gmail.com>
2023-09-21 22:30:21 -0300

    feat: new project init

---
+++ psn-store/src/app/app-routing.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+	declarations: [],
+	exports: [RouterModule],
+})
+export class AppRoutingModule {}
